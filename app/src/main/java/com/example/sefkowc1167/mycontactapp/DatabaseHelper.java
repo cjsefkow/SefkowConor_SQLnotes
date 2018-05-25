d92a9af16f87bf57e636df0035a8706d4a70bd23
cjsefkow <cjsefkow@gmail.com>
2018-05-25 09:27:43 -0700

    Added contact adding methods

--- app/src/main/java/com/example/sefkowc1167/mycontactapp/DatabaseHelper.java
+++ app/src/main/java/com/example/sefkowc1167/mycontactapp/DatabaseHelper.java
@@ -2,13 +2,14 @@ package com.example.sefkowc1167.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "Contact2018.db";
     public static final String TABLE_NAME = "Contact2018_table";
     public static final String ID = "ID";
@@ -19,10 +20,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + TABLE_NAME + " (" +
-                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    COLUMN_NAME_CONTACT + "TEXT, " +
-                    COLUMN_NAME_PHONE + "TEXT, " +
-                    COLUMN_NAME_ADDRESS + "TEXT)";
+                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_NAME_CONTACT + " TEXT, " +
+                    COLUMN_NAME_PHONE + " TEXT, " +
+                    COLUMN_NAME_ADDRESS + " TEXT)";
+
 
 
     public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
@@ -42,17 +44,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.d("MyContactApp", "Databasehelper: upgrading database");
-        db.execSQL(SQL_CREATE_ENTRIES);
+        db.execSQL(SQL_DELETE_ENTRIES);
         onCreate(db);
     }
 
-    public boolean insertData(String column, String item) {
+    public boolean insertData(String name, String address, String phone) {
         Log.d("MyContactApp", "Databasehelper: inserting data");
         SQLiteDatabase db = this.getWritableDatabase();
-        android.content.ContentValues contentValue = new android.content.ContentValues();
-        contentValue.put(column, item);
+        Log.d("MyContactApp", SQL_CREATE_ENTRIES);
+        ContentValues contentValue = new ContentValues();
+        Log.d("MyContactApp", name + " " + address + " " + phone);
+        contentValue.put(COLUMN_NAME_CONTACT, name);
+        contentValue.put(COLUMN_NAME_ADDRESS, address);
+        contentValue.put(COLUMN_NAME_PHONE, phone);
 
         long result = db.insert(TABLE_NAME, null, contentValue);
+        Log.d("MyContactApp", "created result");
         if (result == -1) {
             Log.d("MyContactApp", "Databasehelper: contact insert failed");
             return false;
@@ -62,5 +69,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    public Cursor getAllData() {
+        Log.d("MyContactApp", "MainActivity: calling getAllData");
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
 }
 
--- app/src/main/java/com/example/sefkowc1167/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/sefkowc1167/mycontactapp/MainActivity.java
@@ -1,19 +1,71 @@
 package com.example.sefkowc1167.mycontactapp;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
+    EditText editName;
+    EditText editAddress;
+    EditText editPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editName = findViewById(R.id.editText_name);
+        editAddress = findViewById(R.id.editText_address);
+        editPhone = findViewById(R.id.editText_phone);
+
         myDb = new DatabaseHelper(this);
         Log.d("MyContactApp", "MainActivity: instantiated. DBHelper");
+    }
+
+    public void addData(View view) {
+        Log.d("MyContactApp", "MainActivity: Add contact button pressed");
+        Log.d("MyContactApp", editName.getText().toString());
+        Log.d("MyContactApp", editAddress.getText().toString());
+        Log.d("MyContactApp", editPhone.getText().toString());
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAddress.getText().toString(), editPhone.getText().toString());
+
+        if (isInserted) {
+            Toast.makeText(MainActivity.this, "Success, contact inserted", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Failed, contact not inserted", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void viewData (View view) {
+        Cursor res = myDb.getAllData();
+        Log.d("MyContactApp", "MainActivity: viewData: received cursor " + res.getCount());
+        if (res.getCount() == 0) {
+            showMessage("Error", "no data found in database");
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            //append res column 0,1,2,3 to buffer, delimited by "\n"
+            //TODO
+        }
+        Log.d("MyContactApp", "MainActivity: assembled StringBuffer");
+        showMessage("Data", buffer.toString());
+    }
+
+    public void showMessage(String title, String message) {
+        Log.d("MyContactApp", "MainActivity: showMessage: building alert dialog");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
